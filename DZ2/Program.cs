fc3466174986cd7a6d58fa7a93980d6707132dee
Admin <ge1983@yandex.ru>
2023-03-14 21:15:59 +1000

    Добавлено задача 2 для ДЗ

---
+++ DZ2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+
+int[] InputArray (int length)
+{
+    int[] array = new int[length];
+    Random randon = new Random();
+    for (int i = 0; i<length; i++)
+    {
+        array[i] = randon.Next(1, 30);
+    }
+    return array;
+
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i<=array.Length-1; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int sumToArray (int[] array)
+{
+    int sum = 0;
+    int index = 1;
+    while (index < array.Length)
+    {
+        sum = sum + array[index];
+        index = index +2;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Полученный массив");
+int[] array = InputArray(8);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел на нечетных местах в массиве - {sumToArray (array)}");
